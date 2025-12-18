ca1d4c44578487bec9b01cafd0b3829fc5ce9873
bala-amypo <bala@amypo.in>
2025-12-18 16:52:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ApprovalAction.java
@@ -0,0 +1,9 @@
+package com.exmaple.demo.entity
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table
+public class ApprovalAction{
+    @Id
+    @GeneratedValue(strategy)
+}
