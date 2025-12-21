4077ffa16e00656c8b45df8fbb9b3f6c7858d9f6
bala-amypo <bala@amypo.in>
2025-12-21 17:56:44 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import com.example.demo.exception.ResourceNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
