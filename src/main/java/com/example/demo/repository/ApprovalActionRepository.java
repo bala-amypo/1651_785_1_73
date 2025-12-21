49892ea7fbcd0793e0df93a7ec3e4b7d0baf62ea
bala-amypo <bala@amypo.in>
2025-12-21 11:57:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/ApprovalActionRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.ApprovalAction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ApprovalActionRepository
+        extends JpaRepository<ApprovalAction, Long> {
+
+    List<ApprovalAction>
+    findByLevelAndAction(Integer levelNumber, String action);
+}
