a52eab8371e5c97e72f24c4a127078c6502912de
bala-amypo <bala@amypo.in>
2025-12-29 04:55:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,31 +1,31 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import com.example.demo.model.User;
-// import com.example.demo.service.UserService;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-// @RestController
-// @RequestMapping("/api/users")
-// public class UserController {
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
 
-//     private final UserService userService;
+    private final UserService userService;
 
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-//     @PostMapping("/register")
-//     public ResponseEntity<User> registerUser(@RequestBody User user,
-//                                              @RequestParam("role") String roleName) {
-//         User saved = userService.registerUser(user, roleName);
-//         return new ResponseEntity<>(saved, HttpStatus.CREATED);
-//     }
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user,
+                                             @RequestParam("role") String roleName) {
+        User saved = userService.registerUser(user, roleName);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
 
-//     @GetMapping("/{username}")
-//     public ResponseEntity<User> getByUsername(@PathVariable String username) {
-//         User user = userService.findByUsername(username);
-//         return ResponseEntity.ok(user);
-//     }
-// }
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getByUsername(@PathVariable String username) {
+        User user = userService.findByUsername(username);
+        return ResponseEntity.ok(user);
+    }
+}
